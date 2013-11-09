6fb5da1a7f381faa438df7a01b973cf6245883aa
Po-Ying Chen <poying.me@gmail.com>
2013-11-09 08:18:21 +0800

    install express

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-// https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
-require('nko')('HaySExiRGAQFvfJk');
+'use strict';
 
-var isProduction = (process.env.NODE_ENV === 'production');
+var express = require('express');
 var http = require('http');
-var port = (isProduction ? 80 : 8000);
+var path = require('path');
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/touchday" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+var app = express();
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
