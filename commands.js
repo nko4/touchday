ba98a3941b1aabd83b58121d29e019528730f552
Justin <yuting1987@gmail.com>
2013-11-11 00:01:14 +0800

    add auto_task

--- commands.js
+++ commands.js
@@ -47,5 +47,131 @@ module.exports = function (io, models) {
     });
   };
 
+  commands.sendTaskA = function (cb) {
+    cb || (cb = function () {});
+    console.log('testA');
+    TaskClass.create({
+      name: 'github commit',
+      message: 'Don\'t forget to commit your changes to .gitignore!',
+      url: 'github\.com',
+      value: 'github',
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskA = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskA');
+    TaskClass.create({
+      name: 'github commit',
+      message: 'Don\'t forget to commit your changes to .gitignore!',
+      url: 'github\.com',
+      value: 'github',
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskB = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskB');
+    TaskClass.create({
+      name: 'cat hungry',
+      message: 'I\'m hungry',
+      url: 'fish',
+      value: null,
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskC = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskC');
+    TaskClass.create({
+      name: 'dropbox',
+      message: 'I live push files to dropbox, do you?',
+      url: 'dropbox',
+      value: null,
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskD = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskD');
+    TaskClass.create({
+      name: 'NodeJS',
+      message: 'Do you know last version of nodejs?',
+      url: 'nodejs',
+      value: null,
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskE = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskE');
+    TaskClass.create({
+      name: 'TouchCat',
+      message: 'Do you like TouchCat?',
+      url: 'nodeknockout.com\/teams\/touchday',
+      value: 'Taipei',
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
+  commands.sendTaskF = function (cb) {
+    cb || (cb = function () {});
+    console.log('sendTaskD');
+    TaskClass.create({
+      name: 'vote',
+      message: 'Oh, do not forget to vote!',
+      url: 'nodeknockout.com',
+      value: 'TouchDay',
+      type: 'url',
+      exp: -100
+    }, function (err, taskClass) {
+      if (err) {
+        return cb(err);
+      }
+      commands.emit(taskClass);
+    });
+  };
+
   return commands;
 };
--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var db = require('./lib/db');
 var routes = require('./lib/routes');
 var app = require('./lib/app');
 var events = require('./lib/events');
-var config = require('./config.json');
+if(process.argv[2]==='test'){
+  var config = require('./config.test.json');
+}else{
+  var config = require('./config.json');
+}
 var repl = require('./console');
 
 // connect database and create models
@@ -38,9 +42,27 @@ var server = http.createServer(app).listen(app.get('port'), config.host, functio
   // init socket.io
   var io = events(path.join(__dirname, 'events'), server);
 
+  var commands = require('./commands')(io, models)
+
   repl({
     io: io,
     models: models,
-    commands: require('./commands')(io, models)
+    commands: commands
   });
+
+  var auto_task = function(){
+    var R = Math.floor(Math.random() * 20)
+    switch(R){
+      case 1: commands.sendTaskA(); break;
+      case 3: commands.sendTaskB(); break;
+      case 5: commands.sendTaskC(); break;
+      case 6: commands.sendTaskD(); break;
+      case 7: commands.sendTaskE(); break;
+      case 11: commands.sendTaskF(); break;
+    }
+    console.log('auto_task send:', R)
+    setTimeout(auto_task, 3000);
+  }
+  auto_task();
+
 });
